d8a33691c7a2359ab1244800631ce3f585873231
Unknown <nolse16@student.aau.dk>
2017-11-17 22:29:12 +0100

    Movement mikkel?

--- CreateGameJam2017/Assets/Scripts/Player.cs
+++ CreateGameJam2017/Assets/Scripts/Player.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+
+
+
 public class Player : MonoBehaviour {
+
+    Rigidbody rb;
+    Transform tm;
+    Vector3 input = new Vector3();
+    float movespeed;
+
 
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
+        tm = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //x,y,z
+
+        movespeed = Time.deltaTime * 1000;
+        input = GetInput();
+        //use this to move player
 	}
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(input * movespeed);
+    }
+
+    Vector3 GetInput()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        return new Vector3(x, 0, z);
+    }
 }
+   
+
