2df428e8efd20d918602eafde163b181bcb76f77
Unknown <mlau16@student.aau.dk>
2017-11-17 22:53:22 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ CreateGameJam2017/Assets/Scripts/Gun.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public Transform bulletOrigin;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
