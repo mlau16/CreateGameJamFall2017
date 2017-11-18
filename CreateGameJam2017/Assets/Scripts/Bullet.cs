1a09495abc56dfb0f1ab196ebaad5404fec45b1b
Unknown <mlau16@student.aau.dk>
2017-11-18 01:26:17 +0100

    Gilette

---
+++ CreateGameJam2017/Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    Guntypes gunType;
+    float bulletVel;
+    float damage;
+
+    // Use this for initialization
+    void Start () {
+        gunType = GameObject.FindGameObjectWithTag("Player").GetComponent<Gun>().currentGun;
+        bulletVel = gunType.bulletVel;
+        damage = gunType.damage;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- CreateGameJam2017/Assets/Scripts/Gun.cs
+++ CreateGameJam2017/Assets/Scripts/Gun.cs
@@ -4,16 +4,76 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour {
 
-    public Transform bulletOrigin;
-    
+    private void Awake()
+    {
+        amountOfGuns = gunType.Length;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
+    public Guntypes[] gunType;
+    [HideInInspector]
+    public Guntypes currentGun;
+    float shootingTimer;
+    int amountOfGuns;
+    int activegun;
+
+    void Start ()
+    {
+        shootingTimer = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        
+        ChooseWeapon();
+        FireBullet();
+    }
+
+    void FireBullet()
+    {
+        shootingTimer -= Time.deltaTime;
+
+        if (Input.GetMouseButton(0))
+        {
+            if(shootingTimer <= 0)
+            {
+                // Shoot please
+                //Bullet bullet = Instantiate() as bullet;
+                //shootingTimer = shootingSpeed;
+            }
+        }
+    }
+
+    void ChooseWeapon()
+    {
+        if (Input.GetKey("1"))
+        {
+            activegun = 0;
+            currentGun = gunType[0];
+        }
+        if (Input.GetKey("2"))
+        {
+            activegun = 1;
+            currentGun = gunType[1];
+        }
+        if (Input.GetKey("3"))
+        {
+            activegun = 2;
+            currentGun = gunType[2];
+        }
+
+    }
+}
+
+[System.Serializable]
+public class Guntypes
+{
+    public string name;
+    public float shootingRate;
+    public GameObject gunModel;
+    public Transform bulletOrigin;
+    public GameObject bulletPrefab;
+    public float bulletVel;
+    public float damage;
+
 }
